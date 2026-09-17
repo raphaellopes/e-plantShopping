848e4211b742d8712c3018aa38e15175a1d5feb0
Raphael Lopes <ph.campanario07@gmail.com>
2026-09-17 10:50:06 -0300

    implement increment and decrement logic and show the total cost from CartItem

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,28 +9,51 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    const total = cart.reduce(
+      (acc, item) => acc + parseInt(item.cost.substring(1)) * item.quantity,
+      0
+    );
+
+    return total;
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e);
   };
 
 
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({
+      name: item.name,
+      quantity: item.quantity + 1
+    }))
   };
 
   const handleDecrement = (item) => {
-   
+    if (item.quantity > 0) {
+      dispatch(updateQuantity({
+        name: item.name,
+        quantity: item.quantity - 1
+      }));
+    } else {
+      handleRemove(item);
+    }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    const total = parseInt(item.cost.substring(1)) * item.quantity;
+    return total;
   };
+
+  const handleCheckoutShopping = () => {
+      alert('Functionality to be added for future reference');
+  }
 
   return (
     <div className="cart-container">
@@ -57,7 +80,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={handleCheckoutShopping}>Checkout</button>
       </div>
     </div>
   );
