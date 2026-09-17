df2f23885c6291b90747d6927fb8e9ea16743c71
Raphael Lopes <ph.campanario07@gmail.com>
2026-09-17 11:46:38 -0300

    show total items added from header and ensure item will be removed when it's removed from cart

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -3,7 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { removeItem, updateQuantity } from './CartSlice';
 import './CartItem.css';
 
-const CartItem = ({ onContinueShopping }) => {
+const CartItem = ({ onContinueShopping, onRemoveItem }) => {
   const cart = useSelector(state => state.cart.items);
   const dispatch = useDispatch();
 
@@ -44,6 +44,7 @@ const CartItem = ({ onContinueShopping }) => {
 
   const handleRemove = (item) => {
     dispatch(removeItem(item.name));
+    onRemoveItem(item.name);
   };
 
   // Calculate total cost based on quantity for an item
--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import { addItem } from './CartSlice';
 import CartItem from './CartItem';
@@ -10,6 +10,7 @@ function ProductList({ onHomeClick }) {
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
     const dispath = useDispatch();
+    const cart = useSelector(state => state.cart.items);
 
     const plantsArray = [
         {
@@ -268,6 +269,18 @@ function ProductList({ onHomeClick }) {
       }));
     }
 
+    const handleRemoveItemFromCart = (itemName) => {
+      setAddedToCart((prevState) => ({
+        ...prevState,
+        [itemName]: false
+      }))
+    }
+
+    const getTotalItemsAdded = () => {
+      const totalItems = cart.reduce((acc, item) => acc + item.quantity, 0);
+      return totalItems;
+    }
+
     return (
         <div>
             <div className="navbar" style={styleObj}>
@@ -285,7 +298,14 @@ function ProductList({ onHomeClick }) {
                 </div>
                 <div style={styleObjUl}>
                     <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                    <div>
+                      <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}>
+                        <h1 className='cart'>
+                          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" id="mainIconPathAttribute"></path></svg>
+                          <span className="cart_quantity_count">{getTotalItemsAdded()}</span>
+                        </h1>
+                      </a>
+                    </div>
                 </div>
             </div>
             {!showCart ? (
@@ -323,7 +343,10 @@ function ProductList({ onHomeClick }) {
                   ))}
                 </div>
             ) : (
-                <CartItem onContinueShopping={handleContinueShopping} />
+                <CartItem
+                  onContinueShopping={handleContinueShopping}
+                  onRemoveItem={handleRemoveItemFromCart}
+                />
             )}
         </div>
     );
